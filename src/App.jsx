da891f52b02fdd3d8ae828a65b48f693ca48a344
Anas <anes85594@gmail.com>
2023-10-14 14:12:59 +0000

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './styles/universal.css';
 import Main from './pages/Main';
 import MoviePage from './pages/MoviePage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      {/*<Main />*/}
-      <MoviePage />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/moviepage' element={<MoviePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
